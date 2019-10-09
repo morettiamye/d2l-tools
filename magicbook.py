c1d9fd124cf658bedaca9720c3dffadd44cee2f3
Amy <amyemoretti@gmail.com>
2019-10-09 11:55:35 -0500

    pseudo coding things

--- magicbook.py
+++ magicbook.py
@@ -1,2 +1,19 @@
+# Goals for Magic Book Script
+# Read CSVs and output single formatted CSV
+
+# Concatenate this information from CSV:
+# OUID - OUID, Course Title
+# Term, Session, Acad Group, Subject, Catalog, Section, Mode, ID, Name, Email
+# Into this information:
+# Course Code = Subject + Catalog + Section + Term
+# Instructor Name: First + Last
+# ITC
+# ID
+# Deadline
+# Start Date
+# End Date
+# Asc Build = Subject + Catalog + Last Name
+
+# When concatenating, join with "_"
 
 
