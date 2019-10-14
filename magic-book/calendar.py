00ad24eb4e2b4dbe6a10d5ee134279719b001a55
Amy <amyemoretti@gmail.com>
2019-10-14 13:55:47 -0500

    Working on calendar functions

---
+++ magic-book/calendar.py
@@ -0,0 +1,18 @@
+import datetime
+# timedelta
+
+# Change These Variable Dates for the Applicable School Year
+# Format Year, Month, Day
+fall_registration = datetime.date(2019, 5, 2)
+december_registration = datetime.date(2019, 10, 7)
+winter_registration = datetime.date(2019, 10, 17)
+spring_registration = datetime.date(2020, 2, 6)
+summer_registration = datetime.date(2020, 2, 6)
+summer1_registration = datetime.date(2020, 2, 6)
+summer2_registration = datetime.date(2020, 2, 6)
+
+# reg = datetime.strptime('5/2/2019', '%m/%d/%Y').date()
+# print(reg)
+
+# due = reg + timedelta(weeks=10)
+# print(due)
