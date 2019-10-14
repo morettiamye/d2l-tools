354f5aa74899f091b70b3dce4080872eddcf7889
Amy <amyemoretti@gmail.com>
2019-10-14 15:19:02 -0500

    Updated calendar to use dictionary

--- magic-book/calendar.py
+++ magic-book/calendar.py
@@ -1,59 +1,67 @@
-import datetime
+# Dates
 
+# Change These Dates for the Applicable School Year
+# These dates are correct for the 2019-2020 School Year
 
-# Change These Variable Dates for the Applicable School Year
-# Format Year, Month, Day
+autumn_dates = dict([
+    ("Registration", "5/2/2019"),
+    ("Pull Reports", "7/24/2019"),
+    ("Send Emails", "7/31/2019"),
+    ("Deadline", "8/21/2019"),
+    ("Start Date", "9/11/2019"),
+    ("End Date", "11/26/2019"),
+])
 
-# Autumn
-autumn_registration = datetime.date(2019, 5, 2)
-autumn_pull_reports = datetime.date(2019, 7, 24)
-autumn_send_emails = datetime.date(2019, 7, 31)
-autumn_deadline = datetime.date(2019, 8, 21)
-autumn_start_date = datetime.date(2019, 9, 11)
-autumn_end_date = datetime.date(2019, 11, 26)
+december_dates = dict([
+    ("Registration", "10/17/2019"),
+    ("Pull Reports", "10/17/2019"),
+    ("Send Emails", "10/21/2019"),
+    ("Deadline", "11/11/2019"),
+    ("Start Date", "12/2/2019"),
+    ("End Date", "12/20/2019")
+])
 
-# December
-december_registration = datetime.date(2019, 10, 17)
-december_pull_reports = datetime.date(2019, 10, 17)
-december_send_emails = datetime.date(2019, 10, 21)
-december_deadline = datetime.date(2019, 11, 11)
-december_start_date = datetime.date(2019, 12, 2)
-december_end_date = datetime.date(2019, 12, 20)
+winter_dates = dict([
+    ("Registration", "10/17/2019"),
+    ("Pull Reports", "11/6/2019"),
+    ("Send Emails", "11/13/2019"),
+    ("Deadline", "12/6/2019"),
+    ("Start Date", "1/4/2020"),
+    ("End Date", "3/20/2020"),
+])
 
-# Winter
-winter_registration = datetime.date(2019, 10, 17)
-winter_pull_reports = datetime.date(2019, 11, 6)
-winter_send_emails = datetime.date(2019, 11, 13)
-winter_deadline = datetime.date(2019, 12, 6)
-winter_start_date = datetime.date(2020, 1, 4)
-winter_end_date = datetime.date(2020, 3, 20)
+spring_dates = dict([
+    ("Registration", "2/6/2020"),
+    ("Pull Reports", "2/7/2020"),
+    ("Send Emails", "2/14/2020"),
+    ("Deadline", "3/6/2020"),
+    ("Start Date", "3/28/2020"),
+    ("End Date", "6/12/2020"),
+])
 
-# Spring
-spring_registration = datetime.date(2020, 2, 6)
-spring_pull_reports = datetime.date(2020, 2, 7)
-spring_send_emails = datetime.date(2020, 2, 14)
-spring_deadline = datetime.date(2020, 3, 6)
-spring_start_date = datetime.date(2020, 3, 28)
-spring_end_date = datetime.date(2020, 6, 12)
+summer_dates = dict([
+    ("Registration", "2/6/2020"),
+    ("Pull Reports", "4/27/2020"),
+    ("Send Emails", "5/4/2020"),
+    ("Deadline", "5/25/2020"),
+    ("Start Date", "6/15/2020"),
+    ("End Date", "8/23/2020"),
+])
 
-# Summer
-summer_registration = datetime.date(2020, 2, 6)
-summer_pull_reports = datetime.date(2020, 4, 27)
-summer_send_emails = datetime.date(2020, 5, 4)
-summer_deadline = datetime.date(2020, 5, 25)
-summer_start_date = datetime.date(2020, 6, 15)
-summer_end_date = datetime.date(2020, 8, 23)
+summer1_dates = dict([
+    ("Registration", "2/6/2020"),
+    ("Pull Reports", "4/27/2020"),
+    ("Send Emails", "5/4/2020"),
+    ("Deadline", "5/25/2020"),
+    ("Start Date", "6/15/2020"),
+    ("End Date", "7/19/2020"),
+])
 
-summer1_registration = summer_registration
-summer1_pull_reports = summer_pull_reports
-summer1_send_emails = summer_send_emails
-summer1_deadline = summer_deadline
-summer1_start_date = summer_start_date
-summer1_end_date = datetime.date(2020, 7, 19)
-
-summer2_registration = summer_registration
-summer2_pull_reports = datetime.date(2020, 6, 1)
-summer2_send_emails = datetime.date(2020, 6, 8)
-summer2_deadline = datetime.date(2020, 6, 29)
-summer2_start_date = datetime.date(2020, 7, 20)
-summer2_end_date = datetime.date(2020, 8, 23)
+summer2_dates = dict([
+    ("Registration", "2/6/2020"),
+    ("Pull Reports", "6/1/2020"),
+    ("Send Emails", "6/8/2020"),
+    ("Deadline", "6/29/2020"),
+    ("Start Date", "7/20/2020"),
+    ("End Date", "8/23/2020"),
+])
