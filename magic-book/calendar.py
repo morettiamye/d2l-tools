1a64f05bbd6cd89248bea77538ae15801180ccfc
Amy <amyemoretti@gmail.com>
2019-10-14 14:30:27 -0500

    dates

--- magic-book/calendar.py
+++ magic-book/calendar.py
@@ -1,18 +1,59 @@
 import datetime
-# timedelta
+
 
 # Change These Variable Dates for the Applicable School Year
 # Format Year, Month, Day
-fall_registration = datetime.date(2019, 5, 2)
-december_registration = datetime.date(2019, 10, 7)
+
+# Autumn
+autumn_registration = datetime.date(2019, 5, 2)
+autumn_pull_reports = datetime.date(2019, 7, 24)
+autumn_send_emails = datetime.date(2019, 7, 31)
+autumn_deadline = datetime.date(2019, 8, 21)
+autumn_start_date = datetime.date(2019, 9, 11)
+autumn_end_date = datetime.date(2019, 11, 26)
+
+# December
+december_registration = datetime.date(2019, 10, 17)
+december_pull_reports = datetime.date(2019, 10, 17)
+december_send_emails = datetime.date(2019, 10, 21)
+december_deadline = datetime.date(2019, 11, 11)
+december_start_date = datetime.date(2019, 12, 2)
+december_end_date = datetime.date(2019, 12, 20)
+
+# Winter
 winter_registration = datetime.date(2019, 10, 17)
+winter_pull_reports = datetime.date(2019, 11, 6)
+winter_send_emails = datetime.date(2019, 11, 13)
+winter_deadline = datetime.date(2019, 12, 6)
+winter_start_date = datetime.date(2020, 1, 4)
+winter_end_date = datetime.date(2020, 3, 20)
+
+# Spring
 spring_registration = datetime.date(2020, 2, 6)
+spring_pull_reports = datetime.date(2020, 2, 7)
+spring_send_emails = datetime.date(2020, 2, 14)
+spring_deadline = datetime.date(2020, 3, 6)
+spring_start_date = datetime.date(2020, 3, 28)
+spring_end_date = datetime.date(2020, 6, 12)
+
+# Summer
 summer_registration = datetime.date(2020, 2, 6)
-summer1_registration = datetime.date(2020, 2, 6)
-summer2_registration = datetime.date(2020, 2, 6)
+summer_pull_reports = datetime.date(2020, 4, 27)
+summer_send_emails = datetime.date(2020, 5, 4)
+summer_deadline = datetime.date(2020, 5, 25)
+summer_start_date = datetime.date(2020, 6, 15)
+summer_end_date = datetime.date(2020, 8, 23)
 
-# reg = datetime.strptime('5/2/2019', '%m/%d/%Y').date()
-# print(reg)
+summer1_registration = summer_registration
+summer1_pull_reports = summer_pull_reports
+summer1_send_emails = summer_send_emails
+summer1_deadline = summer_deadline
+summer1_start_date = summer_start_date
+summer1_end_date = datetime.date(2020, 7, 19)
 
-# due = reg + timedelta(weeks=10)
-# print(due)
+summer2_registration = summer_registration
+summer2_pull_reports = datetime.date(2020, 6, 1)
+summer2_send_emails = datetime.date(2020, 6, 8)
+summer2_deadline = datetime.date(2020, 6, 29)
+summer2_start_date = datetime.date(2020, 7, 20)
+summer2_end_date = datetime.date(2020, 8, 23)
