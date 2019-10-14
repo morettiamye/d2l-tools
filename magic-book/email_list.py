290db1df6299ddef3c20111f7e5cb3b9eabd2fd7
Amy <amyemoretti@gmail.com>
2019-10-14 12:23:42 -0500

    Calendar file added

--- magic-book/email_list.py
+++ magic-book/email_list.py
@@ -1,13 +1,13 @@
 import csv
 
+# Change File Path and Course file name here
 file_path = "../test-files/"
 file_name = "email.csv"
 
 def emails():
-    with open(file_path + file_name, "r") as csvfile:
-        email_list = csv.reader(csvfile)
+    with open(file_path + file_name, newline="") as csvfile:
+        email_list = csv.reader(csvfile, delimiter=" ", quotechar="|")
         for row in email_list:
-            print(row)
-            return row
+            print(",".join(row))
 
-emails()
+# emails()
