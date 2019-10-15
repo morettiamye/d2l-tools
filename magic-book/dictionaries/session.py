a00a66cc94891c022f926281d8ef2d9c6b59b438
Amy <amyemoretti@gmail.com>
2019-10-15 15:09:51 -0500

    Session Dict complete

---
+++ magic-book/dictionaries/session.py
@@ -0,0 +1,7 @@
+session = dict([
+    ("1", "Normal (Fall, Winter, Spring, Summer"),
+    ("DEC", "December"),
+    ("SS1", "Summer Session 1"),
+    ("SS2", "Summer Session 2"),
+    ("OLS", "Online Section")
+])
